2357092427ee510cff9590ec1e852eb2fa9a8d13
cctv1005s <384113872@qq.com>
2018-06-06 16:51:50 +0800

    code reconstructor

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const {
   type
 } = image;
 
-
 const arrayData = [];
+// 从Float64Array => Arrary
 data.forEach(item => {
   arrayData.push(item);
 })
@@ -22,77 +22,98 @@ const pixels = _.chunk(arrayData, 4);
  * get width * height matrix
  */
 const matrix = _.chunk(pixels, width);
+
 const zeros = [];
+
 for(let i = 0;i < width; i++){
   zeros.push([0, 0, 0, 1]);
 }
-matrix.unshift(zeros);
-matrix.push(zeros);
+
+// 矩阵开头插入一行0像素
+matrix.unshift(_.cloneDeep(zeros));
+// 矩阵末尾插入一行0像素
+matrix.push(_.cloneDeep(zeros));
 
 matrix.forEach(item => {
   item.push([0, 0, 0, 1]);
   item.unshift([0, 0, 0, 1]);
 });
 
-// 图像锐化 sharpening
+// 图像锐化kernel sharpening
 const sharp = [
   [-1, -1, -1],
   [-1, 9, -1],
   [-1, -1, -1]
 ];
 
-const getMatrix = (width, height, depth) => {
-  let matrix = [];
-  for (let i = 0; i < height; i++) {
-    let m = [];
-    for (let j = 0; j < width; j++) {
-      let de = [];
-      for (let d = 0; d < depth; d++) {
-        de.push(0);
-      }
-      m.push(de);
+// 获得一个全为0的矩阵
+const getMatrix = (...agrs) => {
+  let matrix = 0;
+  while(agrs.length){
+    const length = agrs.pop();
+    const temp = [];
+    for(let i = 0; i < length; i++){
+      temp.push(matrix);
     }
-    matrix.push(m);
+    matrix = temp;
   }
   return matrix;
 };
 
-const newImage = getMatrix(width, height, 4);
-
-const convolution = (kernel, matrix, x, y) => {
-  let sum = 0;
-
-  for (let k = 0; k < 3; k++) {
-    for (let i = 0; i < 3; i++)
-      for (let j = 0; j < 3; j++) {
-        sum += (sharp[i][j] * matrix[x + i + -1][y + j + -1][k]);
-      }
-    newImage[x - 1][y - 1][k] = Math.abs(sum || 0);
+/**
+ * 使用一个卷积核来卷积图像
+ * 
+ * @param {Array} image 待卷积图像
+ * @param {Arrary} kernel 卷积核
+ */
+const convolution = (image, kernel) => {
+  const width = image[0].length - 2;
+  const height = image.length - 2;
+  const newImage = getMatrix(width, height, 4);
+  /**
+   * 两个矩阵块相乘
+   * @param {*} kernel 卷积核
+   * @param {*} matrix 图像矩阵
+   * @param {*} x 当前位置x
+   * @param {*} y 当前位置y
+   */
+  const calc = (kernel, matrix, x, y) => {
+    let sum = 0;
+    for (let k = 0; k < 3; k++) {
+      for (let i = 0; i < 3; i++)
+        for (let j = 0; j < 3; j++) {
+          sum += (kernel[i][j] * matrix[x + i + -1][y + j + -1][k]);
+        }
+      console.log(sum);
+      newImage[x - 1][y - 1][k] = Math.abs(sum || 0);
+    }
+    newImage[x - 1][y - 1][3] = matrix[x][y][3];
   }
-  newImage[x - 1][y - 1][3] = matrix[x][y][3];
+
+  // 开始卷积图像
+  for (let i = 1; i < width + 1; i++) {
+    for (let j = 1; j < height + 1; j++) {
+      calc(kernel, image, i, j);
+    }
+  }
+  return newImage;
 }
 
-// 开始卷积图像
-for (let i = 1; i < width + 1; i++) {
-  for (let j = 1; j < height + 1; j++) {
-    convolution(sharp, matrix, i, j);
-  }
-}
+const newImage = convolution(matrix, sharp);
 
-const ni = _.flattenDeep(newImage);
+const fs = require('fs');
+const cresult = _.flattenDeep(newImage);
 
-const nf = new Float64Array(width * height * 4);
+const cimage = new Float64Array(width * height * 4);
+
 
 for(let i = 0; i < width * height * 4; i++){
-  nf[i] = ni[i];
+  cimage[i] = cresult[i];
 }
-
-
-// fs.writeFileSync('./log/out.json', JSON.stringify(nf, null, 2));
 
 px.write('./output/img1.jpg', {
   height: height,
   width: width,
   type: type,
-  data: nf
+  data: cimage
 });
